6da7778e430141f3098e017af27ca7e103d71c9d
root <www1009054666@qq.com>
2022-08-09 18:28:02 +0800

    hot-02

--- src/main/java/com/xatu/Hello.java
+++ src/main/java/com/xatu/Hello.java
@@ -3,5 +3,7 @@ package com.xatu;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hello,hot-fix");
+        System.out.println("hello,hot-fix");
+        System.out.println("hello,hot-fix");
     }
 }
