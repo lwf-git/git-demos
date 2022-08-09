47d9e9f5fbb5edf6d97d59f50e804faa27ed2ed6
root <www1009054666@qq.com>
2022-08-09 18:31:19 +0800

    04 commit

--- src/main/java/com/xatu/Hello.java
+++ src/main/java/com/xatu/Hello.java
@@ -5,5 +5,6 @@ public class Hello {
         System.out.println("hello,hot-fix");
         System.out.println("hello,hot-fix");
         System.out.println("hello,hot-fix");
+        System.out.println("hello,master----");
     }
 }
